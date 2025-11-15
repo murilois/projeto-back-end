a3e41a0a895abd6b5b2a742e14c9ac945676a4fe
murilois <murilomattosdev05@gmail.com>
2025-11-15 16:34:17 -0300

    Inicializacao do projeto

---
+++ application/movimentacao-service/src/main/java/com/stockcontrol/movimentacao_service/MovimentacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stockcontrol.movimentacao_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentacaoServiceApplication.class, args);
+	}
+
+}
